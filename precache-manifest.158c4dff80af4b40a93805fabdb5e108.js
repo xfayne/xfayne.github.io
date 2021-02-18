d7d84c5139482ea44d176012757ab82e426e91f4
xfayne <xfayne@gmail.com>
2021-02-18 13:52:07 +0200

    Updates

---
+++ precache-manifest.158c4dff80af4b40a93805fabdb5e108.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca42d4fce3425de2c14efdb54281f3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39c4ea5d3a6bf1d6811",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "67e586e54f1d08f74868",
+    "url": "/static/js/2.4feabad6.chunk.js"
+  },
+  {
+    "revision": "e4647edc8b25fb8dc220819ba6e8496d",
+    "url": "/static/js/2.4feabad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39c4ea5d3a6bf1d6811",
+    "url": "/static/js/main.c66a5a4b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0abf4b847aa2c15a22a4946430afcd4c",
+    "url": "/static/media/PredApp.0abf4b84.png"
+  },
+  {
+    "revision": "5fe3e61ce39fe20afdc56dc3c174d3d7",
+    "url": "/static/media/ars.5fe3e61c.png"
+  },
+  {
+    "revision": "2c88bb348591163df1046b173bec01ea",
+    "url": "/static/media/background.2c88bb34.png"
+  },
+  {
+    "revision": "d9e54a76c80b45aa557e6962c51182b0",
+    "url": "/static/media/etiopi.d9e54a76.png"
+  },
+  {
+    "revision": "771d658ee1e44368cf4c6c3f8be6a778",
+    "url": "/static/media/giant.771d658e.png"
+  },
+  {
+    "revision": "9f67f608f24616b13ef944f836732225",
+    "url": "/static/media/girl.9f67f608.png"
+  },
+  {
+    "revision": "9c874ddcefc2cd2f2f0664ecad381858",
+    "url": "/static/media/linkedin_pic.9c874ddc.png"
+  },
+  {
+    "revision": "9cef0351362325131d83517b94a2a55e",
+    "url": "/static/media/table.9cef0351.png"
+  }
+]);
